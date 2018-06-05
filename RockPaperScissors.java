c1a8d6a2bd319e768f666616e3b9f04b955a259d
joe <Joesalmon1985@gmail.com>
2018-06-05 20:25:53 +0100

    Changes to be committed: 	new file:   RockPaperScissors.java

---
+++ RockPaperScissors.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class RockPaperScissors {
+	public static void main( String[] args ) {
+		// The choice of the player as 1,2 or 3 for rock, paper or scissors.
+		int PlayerChoice
+		// The choice of the computer as 1,2 or 3 for rock, paper or scissors.
+		int ComputerChoice
+		// The random number used to select the computer choice
+		int rnd
+
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println( "Choose rock paper scissors\n 1= Rock,\n 2= Paper \n 3= Scissors");
+		pc = keyboard.nextInt();
+		cc = (int )(Math.random() * 2);
+		if ( pc == 1 ) {
+			System.out.println ( "You chose Rock" );
+		}
+		else if ( pc == 2 ) {
+			System.out.println ( "You chose Paper" );
+		}
+		else if ( pc == 3 ) {
+			System.out.println ( "You chose Scissors" );
+		}else {
+			System.out.print("you have not made a valid selection. The game won't work");
+   	
+}
+		System.out.print("random number is" + cc);
+	}
+}
