ce31ee9732f9be99b0d329a3f4168cb5da3c5f99
joe <Joesalmon1985@gmail.com>
2018-06-07 20:59:17 +0100

    modified:   ChooseGame.java

--- ChooseGame.java
+++ ChooseGame.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class ChooseGame {
+	@Override
 	public static void main( String[] args ) {
-	int WantPlay;
+	char WantPlay;
 	System.out.println( "Do you want to play Rock, Paper Scissors? Enter Y or N only.");
 	Scanner keyboard = new Scanner(System.in);
 	WantPlay = keyboard.nextInt();
 	if ( WantPlay == 'Y' ) {
 		System.out.println("Okay lets play");
-		RockPaperScissors.RockPaperScissors ();
+		RockPaperScissors().RockPaperScissors ();
 		}
 	else if ( WantPlay == 'N') {
 		System.out.println("Okay thats fine, goodbye.");
